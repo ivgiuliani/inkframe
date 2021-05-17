bc078873db159530512497d314e854380aef07d2
Ivan Giuliani <giuliani.v@gmail.com>
2021-05-17 21:58:50 +0100

    Improve bitmap generation
    Allows specifying a group of files

--- generate_cpp_bitmap.py
+++ generate_cpp_bitmap.py
@@ -2,34 +2,45 @@
 
 # Simple script to convert binary files to CPP byte arrays.
 
+import os
 import sys
+from pathlib import Path
 
-TEMPLATE = """#ifndef %(def_name)s
+CONTAINER_TEMPLATE = """#ifndef %(def_name)s
 #define %(def_name)s
 #ifdef ESP32
 #  define E_PROGMEM PROGMEM
 #else
 #  define E_PROGMEM
 #endif
-const unsigned char %(name)s[] E_PROGMEM = {
-%(content)s
-};
+%(declarations)s
 #endif
 """
 
-def main(args):
-    var_name, fname = args[1], args[2]
-    with open(fname, "rb", buffering=0) as f:
-        content = f.read()
-        all_bytes = [
-            hex(b) for b in content
-        ]
+DECLARATION_TEMPLATE = """
+const unsigned char %(name)s[] E_PROGMEM = {
+%(content)s
+};
+"""
 
-        print(TEMPLATE % {
-            "def_name": "__%s_H" % var_name.upper(),
-            "name": var_name,
-            "content": ", ".join(all_bytes),
-        })
+def file_declaration(def_name, path: Path):
+    name = def_name.lower() + "_" + path.with_suffix("").name.lower().replace(".", "_")
+
+    with open(path, "rb", buffering=0) as f:
+        content = f.read()
+
+        return DECLARATION_TEMPLATE % {
+            "name": name,
+            "content": ", ".join([hex(b) for b in content]),
+        }
+
+def main(args):
+    def_name, files = args[1], args[2:]
+
+    print(CONTAINER_TEMPLATE % {
+        "def_name": "__%s_H" % def_name.upper(),
+        "declarations": "\n".join([file_declaration(def_name, Path(f)) for f in files]),
+    })
 
 if __name__ == "__main__":
     sys.exit(main(sys.argv))
