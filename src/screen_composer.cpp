47178fad12e6b8dedcb5d8a5450b6bce665a13ae
Ivan Giuliani <giuliani.v@gmail.com>
2021-05-18 22:41:54 +0100

    Rejig the screen to use ubuntu fonts

--- src/screen_composer.cpp
+++ src/screen_composer.cpp
@@ -1,24 +1,25 @@
 #include <Arduino.h>
 #include <HTTPClient.h>
+#include <TimeLib.h>
 
 #include "weather.h"
 #include "tfl.h"
 #include "screen_composer.h"
 #include "Display.h"
 #include "micro_utils.h"
-#include <TimeLib.h>
 
 #include "icons/weather.bmp.h"
 
 // Due to a bug in the GFX library from Adafruit, these must be included *after*
 // the include of display.h
-#include <Fonts/FreeSans9pt7b.h>
-#include <Fonts/FreeSansBold9pt7b.h>
-#include <Fonts/FreeSans12pt7b.h>
-#include <Fonts/FreeSansBold12pt7b.h>
-#include <Fonts/FreeSans24pt7b.h>
-#include <Fonts/FreeMono9pt7b.h>
-#include <Fonts/FreeMonoBold24pt7b.h>
+#include "fonts/UbuntuMonoBold9pt8b.h"
+#include "fonts/UbuntuMonoRegular9pt8b.h"
+#include "fonts/UbuntuMonoBold24pt8b.h"
+#include "fonts/UbuntuRegular9pt8b.h"
+#include "fonts/UbuntuMedium9pt8b.h"
+#include "fonts/UbuntuMedium11pt8b.h"
+#include "fonts/UbuntuBold9pt8b.h"
+#include "fonts/UbuntuBold12pt8b.h"
 
 struct screen_t update_screen_data() {
   struct screen_t screen;
@@ -47,16 +48,16 @@ struct screen_t update_screen_data() {
 
 void draw_tfl_data(Display *display, screen_t screen) {
   // Tfl data sits in the left half of the screen
-  uint16_t height = 20;
+  uint16_t start_height = 85;
 
   std::map<String, String>::iterator it = screen.tube_status.begin();
   while (it != screen.tube_status.end()) {
-    display->set_font(&FreeSansBold9pt7b);
-    display->draw_text(it->first, 40, height);
+    display->set_font(&UbuntuBold9pt8b);
+    display->draw_text(it->first, 40, start_height);
 
-    display->set_font(&FreeSans9pt7b);
-    display->draw_text(it->second, 225, height);
-    height += 32;
+    display->set_font(&UbuntuMedium9pt8b);
+    display->draw_text(it->second, 225, start_height);
+    start_height += 32;
     it++;
   }
 }
@@ -115,47 +116,48 @@ BWBitmap get_weather_icon_small(Weather weather) {
 
 void draw_current_weather(Display *display, weather_t current) {
   BWBitmap bmp = get_weather_icon_large(current.weather);
-  display->draw_bitmap(&bmp, 400, 10);
+  display->draw_bitmap(&bmp, 390, 10);
 
-  display->set_font(&FreeMonoBold24pt7b);
-  display->draw_text(String(current.min_temp_c) + String("C"), 550, 60);
+  display->set_font(&UbuntuMonoBold24pt8b);
+  display->draw_text(String(current.min_temp_c) + String("°"), 540, 60);
 
-  display->set_font(&FreeSansBold12pt7b);
-  display->draw_text(weather_to_description(current.weather), 550, 100);
+  display->set_font(&UbuntuMedium11pt8b);
+  display->draw_text(weather_to_description(current.weather), 540, 100);
 }
 
 void draw_next_3_days_weather(Display *display, std::array<weather_t,3> weather) {
   for (uint8_t i = 0; i < 3; i++) {
     BWBitmap bmp = get_weather_icon_small(weather[i].weather);
 
-    display->draw_bitmap(&bmp, 420 + (i * 110), 150);
+    display->draw_bitmap(&bmp, 410 + (i * 110), 150);
 
-    display->set_font(&FreeMono9pt7b);
-    const String temp_ind = String(weather[i].min_temp_c) + "/" + String(weather[i].max_temp_c) + String("C");
-    display->draw_text(temp_ind, 425 + (i * 110), 240);
+    display->set_font(&UbuntuMedium9pt8b);
+    const String temp_ind = String(weather[i].min_temp_c) + "/" + String(weather[i].max_temp_c) + String("°");
+    display->draw_text(temp_ind, 415 + (i * 110), 240);
   }
 }
 
 void draw_date(Display *display, uint32_t now_utc_timestamp) {
   char buff[11]; // +1 for the `\0` terminator
-  snprintf(buff, sizeof(buff), "%02d/%02d/%d", day(now_utc_timestamp), month(now_utc_timestamp), year(now_utc_timestamp));
+  snprintf(buff, sizeof(buff), "%02d/%02d/%d",
+    day(now_utc_timestamp), month(now_utc_timestamp), year(now_utc_timestamp));
 
-  display->set_font(&FreeMonoBold24pt7b);
-  display->draw_text(buff, 400, 350);
+  display->set_font(&UbuntuMonoBold24pt8b);
+  display->draw_text(buff, 40, 45);
 }
 
 void update_display(Display *display, screen_t screen) {
   display->new_frame();
-
-  draw_tfl_data(display, screen);
-  draw_current_weather(display, screen.current_weather);
-  draw_next_3_days_weather(display, screen.next_three_days_weather);
 
   // As a hack to avoid an extra http request (since there's no RTC on the
   // board), we use the UTC timestamp as returned by the weather call to infer
   // what day we're in. It will be wrong around midnight especially during DST
   // but it's meant to be used in London so will not be too far off normally.
   draw_date(display, screen.current_weather.unix_timestamp_utc);
+
+  draw_tfl_data(display, screen);
+  draw_current_weather(display, screen.current_weather);
+  draw_next_3_days_weather(display, screen.next_three_days_weather);
 
   display->refresh();
 }
