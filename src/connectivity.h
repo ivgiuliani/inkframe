16e33e1b22d2ce37e4d9b6c24fbe2ed108455cc3
Ivan Giuliani <giuliani.v@gmail.com>
2021-05-19 20:43:14 +0100

    Rework wifi connectivity handler
    This new version is more robust and will handle reconnections on
    disconnects or connection timeouts.

--- src/connectivity.h
+++ src/connectivity.h
@@ -9,40 +9,53 @@
 #include "micro_utils.h"
 
 #define WIFI_HOSTNAME QUOTE(_WIFI_HOSTNAME)
-#ifndef _WIFI_SSID
+#if !defined(_WIFI_SSID)
 #  error "No WIFI SSID defined"
 #else
 #  define WIFI_SSID QUOTE(_WIFI_SSID)
 #endif
 
-#ifndef _WIFI_PASS
+#if !defined(_WIFI_PASS)
 #  error "No WIFI pass key defined"
 #else
 #  define WIFI_PASS QUOTE(_WIFI_PASS)
 #endif
 
+#if !defined(WIFI_CONNECTION_TIMEOUT_MS)
+#  define WIFI_CONNECTION_TIMEOUT_MS 30000
+#endif
+
+enum ConnectionInternalState {
+  CI_START = 0,
+  CI_CONNECTING = 1,
+  CI_CONNECTED = 2,
+  CI_DISCONNECTED = 3,
+};
+
 class Connectivity {
 public:
-  void attempt_wifi_connection(uint32_t next_attempt_delay_ms = 30000) {
+  Connectivity(const uint32_t connect_timeout_ms = 30000) :
+    connect_timeout_ms(connect_timeout_ms) {}
+
+  void attempt_wifi_connection() {
     const uint32_t now = millis();
 
     if (std::string(WIFI_SSID).empty()) PANIC(F("Empty Wifi SSID, aborting"));
     if (std::string(WIFI_PASS).empty()) PANIC(F("Empty Wifi password, aborting"));
 
-    if (last_connection_attempt < 0 || now - last_connection_attempt >= next_attempt_delay_ms) {
-      Serial.printf("Attempt connection to WiFi: '%s'...\n", WIFI_SSID);
+    Serial.printf("Attempt connection to WiFi: '%s'...\n", WIFI_SSID);
 
-      WiFi.disconnect(false);
-      WiFi.mode(WIFI_STA);
-      WiFi.hostname(WIFI_HOSTNAME);
-      WiFi.begin(WIFI_SSID, WIFI_PASS);
+    WiFi.disconnect(false);
+    WiFi.mode(WIFI_STA);
+    WiFi.hostname(WIFI_HOSTNAME);
+    WiFi.begin(WIFI_SSID, WIFI_PASS);
 
-      last_connection_attempt = now;
-    }
+    last_connection_attempt = now;
+    state = CI_CONNECTING;
   }
 
   void disconnect() {
-    if (connected) {
+    if (is_connected()) {
       WiFi.disconnect(true);
       WiFi.mode(WIFI_OFF);
     }
@@ -54,15 +67,23 @@ public:
     switch (wifi_status) {
       case WL_CONNECT_FAILED:
       case WL_CONNECTION_LOST:
-      case WL_DISCONNECTED:
       case WL_NO_SSID_AVAIL:
-        if (connected) {
+      case WL_DISCONNECTED:
+        if (connection_attempt_has_timed_out()) {
+          Serial.println("Connection attempt timed out, retrying");
+          attempt_wifi_connection();
+        } else if (is_connected()) {
           Serial.printf("Lost connection to WIFI: %d\n", wifi_status);
-          connected = false;
+          state = CI_DISCONNECTED;
+        }
+        break;
+        if (is_connected()) {
+          Serial.printf("Lost connection to WIFI: %d\n", wifi_status);
+          state = CI_DISCONNECTED;
         }
         break;
       case WL_CONNECTED:
-        if (!connected) {
+        if (!is_connected()) {
           Serial.print("Connected to WiFi, IP address: ");
           Serial.println(WiFi.localIP());
 
@@ -70,11 +91,14 @@ public:
             Serial.println("Couldn't setup MDNS responder.");
           }
         }
-        connected = true;
+        state = CI_CONNECTED;
         return;
       case WL_NO_SHIELD:
       case WL_IDLE_STATUS:
-        // Nothing to do in these cases.
+        if (connection_attempt_has_timed_out()) {
+          Serial.println("Connection attempt timed out, retrying");
+          attempt_wifi_connection();
+        }
         break;
       default:
         Serial.printf("Unknown wifi state: %d\n", wifi_status);
@@ -82,13 +106,19 @@ public:
     }
   }
 
-  bool is_connected() {
-    return connected;
+  inline bool is_connected() {
+    return state == CI_CONNECTED;
   }
 
 private:
-  long last_connection_attempt = -1;
-  bool connected = false;
+  const uint32_t connect_timeout_ms;
+  int64_t last_connection_attempt = -1;
+  ConnectionInternalState state = CI_START;
+
+  bool connection_attempt_has_timed_out() {
+    const int64_t connection_elapsed = millis() - abs(last_connection_attempt);
+    return state == CI_CONNECTING && connection_elapsed >= connect_timeout_ms;
+  }
 };
 
 #endif // __CONNECTIVITY_H
