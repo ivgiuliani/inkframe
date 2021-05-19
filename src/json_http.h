88bbba015a08530e5e98e370a8c77b724a6fd619
Ivan Giuliani <giuliani.v@gmail.com>
2021-05-19 18:28:04 +0100

    Improve comments on json_from_http

--- src/json_http.h
+++ src/json_http.h
@@ -1,5 +1,5 @@
-#ifndef __HTTP_H
-#define __HTTP_H
+#ifndef __JSON_HTTP_H
+#define __JSON_HTTP_H
 
 #include <ArduinoJson.h>
 #include <HTTPClient.h>
@@ -14,15 +14,16 @@
 #  define JSON_HTTP_READ_TIMEOUT 10000
 #endif
 
-// Utility method to return a parsed JSON back from an HTTP response.
-//
-// @param client an instance of the http client that will be used to execute the
-//        http request
-// @param url    the url to execute the request agains
-// @param json   a json document where the response will be parsed into
-// @return true if the HTTP call returns successfully.
+/** Utility method to return a parsed JSON back from an HTTP response.
+ *
+ * @param client an instance of the http client that will be used to execute the
+ *        http request
+ * @param url    the url to execute the request agains
+ * @param json   a json document where the response will be parsed into
+ * @return true if the HTTP call returns successfully.
+ */
 bool json_from_http(HTTPClient *client, const char *url, JsonDocument *json,
   int32_t connect_timeout = JSON_HTTP_CONNECT_TIMEOUT,
   int32_t read_timeout = JSON_HTTP_READ_TIMEOUT);
 
-#endif // __HTTP_H
+#endif // __JSON_HTTP_H
