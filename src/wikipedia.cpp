f56cbf10003971f1f88dec3dae44f39e632b7fe9
Ivan Giuliani <giuliani.v@gmail.com>
2021-05-23 22:02:21 +0100

    Limit the length of the fact of the day
    Temporary until we can mount the screen on a larger frame.

--- src/wikipedia.cpp
+++ src/wikipedia.cpp
@@ -34,11 +34,13 @@ struct wikipedia_onthisday_t wikipedia_get_onthisday(HTTPClient *client,
 
   const uint16_t all_events_count = json["events"].size();
   uint8_t attempt = 0;
+
+  // TODO: remove/increase char limit if mounted on a larger panel
   do {
     const uint16_t idx = random(all_events_count);
     result.year = json["events"][idx]["year"].as<uint16_t>();
     result.text = json["events"][idx]["text"].as<String>();
-  } while (attempt++ < 5 && !replace_invalid_chars(&result.text) && result.text.length() <= 150);
+  } while (attempt++ < 5 && !replace_invalid_chars(&result.text) && result.text.length() <= 130);
 
   return result;
 }
