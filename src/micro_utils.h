a7776cb4c2095dad8f622ca98d7da30b22a1bda5
Ivan Giuliani <giuliani.v@gmail.com>
2021-06-02 21:02:32 +0100

    Support forced inline

--- src/micro_utils.h
+++ src/micro_utils.h
@@ -4,6 +4,19 @@
 #define Q(x) #x
 #define QUOTE(x) Q(x)
 
+#if defined(__GNUC__)
+#  if !defined(__FORCE_INLINE)
+#    define __FORCE_INLINE __attribute__((always_inline))
+#  else
+#    define __FORCE_INLINE inline
+#  endif
+#  if !defined(__FORCE_NO_INLINE)
+#    define __FORCE_NO_INLINE __attribute__((noinline))
+#  else
+#    define __FORCE_NO_INLINE
+#  endif
+#endif
+
 // The default `yield` method does not allow to specify a time period after
 // which to regain control.
 #define YIELD(ms) (vTaskDelay(ms / portTICK_PERIOD_MS))
